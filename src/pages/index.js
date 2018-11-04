e5ce2d667ccd83e6c47107f13120bd3da884b551
Daniel Slatkin <DSlatkin@users.noreply.github.com>
2018-11-04 22:43:15 +0000

    refactored home.js to index.js for app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,36 @@
-import React from 'react'
-import LighthouseForm from '../components/LighthouseForm/LighthouseForm'
+import React, { Component } from 'react'
+import './App.css'
+import AppHeader from '../components/AppHeader'
+import AppHome from '../components/AppHome'
+import AppFooter from '../components/AppFooter'
 
-const Index = () => <LighthouseForm />
+class Home extends Component {
+	componentDidMount() {
+		document.querySelector('#web-hook input').addEventListener('click', this.copyHook)
+		document.querySelector('#web-hook input').addEventListener('keypress', this.copyHook)
+		document.querySelector('#web-hook button').addEventListener('click', this.copyHook)
+	}
 
-export default Index
+	copyHook = e => {
+		e.preventDefault()
+		const message = document.getElementById('copied')
+		if (e.target.tagName === 'INPUT') {
+			// Reset the value in case user gave input
+			e.target.value = 'https://ourwebook.com/'
+		}
+		document.querySelector('#web-hook input').select()
+		document.execCommand('copy')
+	}
+
+	render() {
+		return (
+			<div>
+				<AppHeader />
+				<AppHome />
+				<AppFooter />
+			</div>
+		)
+	}
+}
+
+export default Home
